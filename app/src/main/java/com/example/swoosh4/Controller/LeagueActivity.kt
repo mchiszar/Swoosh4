b0c6b25bad5fa6179e73fe6a9f77229f5e5e2167
mchiszar <52429587+mchiszar@users.noreply.github.com>
2019-07-24 18:16:10 -0500

    lesson 10

--- app/src/main/java/com/example/swoosh4/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh4/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.example.swoosh4.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.View
 import android.widget.Toast
 import com.example.swoosh4.Model.Player
@@ -13,12 +14,25 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun onMensClicked (view: View) {
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+    }
+
+        }
+
+    fun onMensClicked(view: View) {
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
@@ -26,7 +40,7 @@ class LeagueActivity : BaseActivity() {
 
     }
 
-    fun onWomensClicked (view: View) {
+    fun onWomensClicked(view: View) {
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
@@ -34,7 +48,7 @@ class LeagueActivity : BaseActivity() {
 
     }
 
-    fun onCoedClicked (view: View) {
+    fun onCoedClicked(view: View) {
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
@@ -42,9 +56,9 @@ class LeagueActivity : BaseActivity() {
 
     }
 
-    fun leagueNextClicked (view: View) {
+    fun leagueNextClicked(view: View) {
         if (player.league != "") {
-            val skillActivity = Intent (this, SkillActivity::class.java)
+            val skillActivity = Intent(this, SkillActivity::class.java)
             skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         } else {
